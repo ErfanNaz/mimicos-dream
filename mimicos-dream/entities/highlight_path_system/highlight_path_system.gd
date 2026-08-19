885b8a3eba76dc37f4c242d37c3ad5d3950e3aa9
erfan <erfan.nazm@gmail.com>
2026-08-19 18:40:08 +0200

    updated map 01

--- mimicos-dream/entities/highlight_path_system/highlight_path_system.gd
+++ mimicos-dream/entities/highlight_path_system/highlight_path_system.gd
@@ -1,28 +1,57 @@
 class_name CameraPathSystem extends Node3D
 
-@export var camera_target_path_follow_3d: PathFollow3D
-@export var camera_position_path_follow_3d: PathFollow3D
+@export var camera_position_curve: Curve3D
+@export var camera_target_curve: Curve3D
 
-@export var camera_target: Node3D
-@export var camera_position_target: Node3D
+@export var camera_p_path_follow_3d: Path3D
+@export var camera_t_path_follow_3d: Path3D
 
 @export var interact_data: InteractData
 
-@export_range(0, 100, 1, "prefer_slider") var toggle_back_timer: int = 0
+@export_range(0, 100, 1, "prefer_slider") var playback_back_timer: int = 0
+@export_range(0, 100, 1, "prefer_slider") var camera_target_back_timer: int = 0
 
 @export var one_shot: bool = false
 
 @export_category("Internal")
 @export var trigger_switch: TriggerSwitch
 @export var phantom_camera_3d: PhantomCamera3D
+@export var camera_target_path_follow_3d: PathFollow3D
+@export var camera_position_path_follow_3d: PathFollow3D
+@export var camera_target: Node3D
+@export var camera_position_target: Node3D
+@export var camera_target_path_3d: Path3D
+@export var camera_position_path_3d: Path3D
 
 var _is_playing: bool = false
 
 func _ready() -> void:
-	trigger_switch.toggle_back_timer = toggle_back_timer
+	trigger_switch.toggle_back_timer = playback_back_timer
+	if camera_target_back_timer == 0:
+		camera_target_back_timer = playback_back_timer
 	if interact_data:
 		trigger_switch.interaction.interact_data = interact_data
 	trigger_switch.on_switch.connect(self._on_switch_toggle)
+	_setup_path()
+	
+func _setup_path() -> void:
+	if camera_p_path_follow_3d:
+		camera_position_path_3d.curve = camera_p_path_follow_3d.curve
+	elif camera_position_curve:
+		camera_position_path_3d.curve = camera_position_curve
+	else:
+		ApplicationManager.warn("missing path3D or Curve3D")
+		camera_position_curve = Curve3D.new()
+		camera_position_path_3d.curve = camera_position_curve
+		
+	if camera_t_path_follow_3d:
+		camera_target_path_3d.curve = camera_t_path_follow_3d.curve
+	elif camera_target_curve:
+		camera_target_path_3d.curve = camera_target_curve
+	else:
+		ApplicationManager.warn("missing path3D or Curve3D")
+		camera_position_curve = Curve3D.new()
+		camera_target_path_3d.curve = camera_position_curve
 
 func _on_switch_toggle(player_controller: PlayerController, on: bool) -> void:
 	if on:
@@ -53,14 +82,14 @@ func start() -> void:
 		camera_target_path_follow_3d,
 		"progress_ratio",
 		1.0,
-		toggle_back_timer
+		camera_target_back_timer
 	)
 
 	tween.tween_property(
 		camera_position_path_follow_3d,
 		"progress_ratio",
 		1.0,
-		toggle_back_timer
+		playback_back_timer
 	)
 
 	tween.set_parallel(false)
