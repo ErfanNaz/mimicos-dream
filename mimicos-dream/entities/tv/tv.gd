bb62eaf5460aa46de6a92a753165aae17ec74f71
erfan <erfan.nazm@gmail.com>
2026-08-27 17:00:40 +0200

    updated letter and tv system

--- mimicos-dream/entities/tv/tv.gd
+++ mimicos-dream/entities/tv/tv.gd
@@ -2,15 +2,22 @@ extends Node3D
 
 @export_category("Internal")
 @export var video_stream_player: VideoStreamPlayer
-@export var trigger_switch: TriggerSwitch
+@export var interaction: Interaction
 
 var turned_on: bool = false
 
+var is_on: bool = false
+var can_switch: bool = true
+
 func _ready() -> void:
-	trigger_switch.on_switch.connect(self._on_switch_toggle)
+	interaction.on_interact.connect(self._on_switch_toggle)
 	
-func _on_switch_toggle(player_controller: PlayerController, on: bool) -> void:
-	if on:
+func _on_switch_toggle(player_controller: PlayerController) -> void:
+	if !can_switch:
+		return
+	can_switch = false
+	is_on = !is_on
+	if is_on:
 		if !turned_on:
 			video_stream_player.play()
 			turned_on = true
@@ -18,3 +25,5 @@ func _on_switch_toggle(player_controller: PlayerController, on: bool) -> void:
 			video_stream_player.paused = false
 	else:
 		video_stream_player.paused = true
+	await get_tree().create_timer(1).timeout
+	can_switch = true
