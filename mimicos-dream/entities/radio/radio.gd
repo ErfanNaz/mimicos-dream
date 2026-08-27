2980c918ac8a0cb0f69b06c9ea5358ef4fe1afed
erfan <erfan.nazm@gmail.com>
2026-08-27 13:54:23 +0200

    updated radio

--- mimicos-dream/entities/radio/radio.gd
+++ mimicos-dream/entities/radio/radio.gd
@@ -4,10 +4,18 @@ extends Node3D
 
 var pause_position := 0.0
 
+var is_on: bool = false
+var can_switch: bool = true
 
-func _on_trigger_switch_on_switch(player_controller: PlayerController, on: bool) -> void:
-	if on:
+func _on_interaction_on_interact(player_controller: PlayerController) -> void:
+	if !can_switch:
+		return
+	can_switch = false
+	is_on = !is_on
+	if is_on:
 		audio_stream_player_3d.play(pause_position)
 	else:
 		pause_position = audio_stream_player_3d.get_playback_position()
 		audio_stream_player_3d.stop()
+	await get_tree().create_timer(1).timeout
+	can_switch = true
